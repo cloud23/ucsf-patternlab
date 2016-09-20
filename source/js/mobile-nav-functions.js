0be907a2047c7a17284dad8d38ff5f79358d5623
Patz Matias <patrick.matias@flags.fm>
2016-09-20 14:01:45 +0800

    Ticket #34 finished mobile search form and toggles, did polishing on nav-global and nav-main parts

--- source/js/mobile-nav-functions.js
+++ source/js/mobile-nav-functions.js
@@ -4,39 +4,52 @@ $(function(){
 	$mobileLinksToggle = $('.nav-main-mobile ul li a, .nav-main-mobile ul li .display-arrow'),
 	$navMainMobile = $('.nav-main-mobile');
 
-	$mobileMenuToggle.on('click', function(e){
-		e.preventDefault();
-		$navMainMobile.toggleClass('open');
+	var mobileNavEvents = {
+		init: function(){
+			this.menuToggle();
+			this.linksToggle();
+		},
+		menuToggle: function() {
+			// Menu Toggle
+			$mobileMenuToggle.on('click', function(e){
+				e.preventDefault();
+				$navMainMobile.toggleClass('open');
 
-		if($(this).hasClass('active')) {
-			$navMainMobile.attr('aria-hidden',false);
-		} else {
-			$navMainMobile.attr('aria-hidden',true);
-		}
-	});
-
-	$mobileLinksToggle.on('click', function(e) {
-		e.preventDefault();
-		$this = $(this),
-		$parent = $this.parent(),
-		$popupState = $this.parent().attr('aria-haspopup');
-		var link = $this.attr('href');
-
-		if($parent.hasClass('active')) {
-			$parent.removeClass('active').find('.display-arrow').removeClass('active').parent().find('.dropdown-menu').removeClass('open');
-
-		} else {
-			$mobileLinksToggle.parent().parent().find('.active').each(function(){
-				$(this).removeClass('active').find('.display-arrow').removeClass('active').parent().find('.dropdown-menu').removeClass('open');
+				if($(this).hasClass('active')) {
+					$navMainMobile.attr('aria-hidden',false);
+				} else {
+					$navMainMobile.attr('aria-hidden',true);
+				}
 			});
+		},
+		linksToggle: function() {
+			// Links Toggle
+			$mobileLinksToggle.on('click', function(e) {
+				e.preventDefault();
+				$this = $(this),
+				$parent = $this.parent(),
+				$popupState = $this.parent().attr('aria-haspopup');
+				var link = $this.attr('href');
+
+				if($parent.hasClass('active')) {
+					$parent.removeClass('active').find('.display-arrow').removeClass('active').parent().find('.dropdown-menu').removeClass('open');
+
+				} else {
+					$mobileLinksToggle.parent().parent().find('.active').each(function(){
+						$(this).removeClass('active').find('.display-arrow').removeClass('active').parent().find('.dropdown-menu').removeClass('open');
+					});
 
 
-			if( $this.parent().find('ul').length > 0 ) {
-				$this.parent().toggleClass('active').find('.display-arrow').toggleClass('active').parent().find('.dropdown-menu').toggleClass('open');
-			} else {
-				window.location.href = link;
-			}
+					if( $this.parent().find('ul').length > 0 ) {
+						$this.parent().toggleClass('active').find('.display-arrow').toggleClass('active').parent().find('.dropdown-menu').toggleClass('open');
+					} else {
+						window.location.href = link;
+					}
+				}
+			} );
 		}
+	};
 
-	} );
+	if( $mobileMenuToggle.length > 0 ) mobileNavEvents.init();
+
 });
