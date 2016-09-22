0e8b603208bbbf94da4296d8d27a1183b194c255
Norman Manantan <norman@flags.fm>
2016-09-22 16:13:17 +0800

    Removed calling of shell:pattern_lab_require_styleguidekit_assets during init

--- grunt/tasks/init.js
+++ grunt/tasks/init.js
@@ -3,7 +3,6 @@ module.exports = function(grunt) {
     'update',
     'shell:pattern_lab_require_patternengine',
     'shell:pattern_lab_require_styleguidekit_default',
-    'shell:pattern_lab_require_styleguidekit_assets',
     'notify:build',
     ]);
 };
