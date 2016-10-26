ec9642b78bde75ff5377612bb4738e7a5eef7fde
Patz Matias <patrick.matias@flags.fm>
2016-10-26 14:03:51 +0800

    Ticket #211 Fix for shadow bug

--- source/js/nav-behavior.js
+++ source/js/nav-behavior.js
@@ -2,13 +2,13 @@ jQuery(function($){
 	var $window = $(window), $body = $('body'), $nav = $('.nav--wrapper'), $content = $('.contents--wrapper');
 	var navHeight = ($('body').hasClass('transparent')) ? $nav.find('.nav-global--wrapper').height() : $nav.height();
 	var firstElementHeight = $content.find('div:first').height();
-	var threshold = (navHeight+firstElementHeight);
-	var thresholdHide = (navHeight+firstElementHeight)*0.95;
+	var threshold = navHeight+firstElementHeight;
+	var thresholdHide = threshold*0.95;
 
 	if($window.width() >= 1024) {
 		if($body.hasClass('transparent')) {
 			// console.log(threshold);
-			$window.on('scroll',function(e){
+			$window.scroll(function(e){
 				var scrollTop = $(this).scrollTop();
 				if(scrollTop >= threshold) {
 
@@ -19,11 +19,12 @@ jQuery(function($){
 								$(this).removeAttr('style');
 						});
 					}
-				} else if (scrollTop <= thresholdHide ) {
+				} else {
 					if(!$body.hasClass('transparent')) {
 						$body.addClass('transparent');
 						$nav.removeClass('scrolling');
 						$content.removeAttr('style');
+
 					}
 				}
 
@@ -32,18 +33,17 @@ jQuery(function($){
 		} else {
 			$nav.css({'position':'absolute'}).addClass("static");
 			thresholdHide = threshold*0.50;
-			$window.on('scroll',function(e){
+			$window.scroll(function(e){
 				var scrollTop = $(this).scrollTop();
 				if(scrollTop >= threshold) {
 					if($nav.hasClass('static')) {
-						$nav.css({'position':'fixed'}).css({'top':'-'+$nav.height()+'px'}).animate({'top':'0'},300,'linear',function(){
-									$(this).removeAttr('style').removeClass('static').addClass('scrolling');
-							});
+						$nav.removeAttr('style').removeClass('static').addClass('scrolling');
+						$nav.css({'position':'fixed'}).css({'top':'-'+$nav.height()+'px'}).animate({'top':'0'},300,'linear');
 						$content.css({'margin-top':$nav.height()+'px'});
 					}
-				} else if (scrollTop <= thresholdHide ) {
-					if(!$nav.hasClass('static')) {
-						$nav.css({'position':'absolute'}).addClass("static").removeClass("scrolling");
+				} else {
+					if($nav.hasClass('scrolling')) {
+						$nav.removeClass('scrolling').addClass("static").css({'position':'absolute'});
 						$content.removeAttr('style');
 					}
 				}
